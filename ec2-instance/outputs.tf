4ce1f5ca1c4271a5022d25091babfc2b29b1735b
pmi-git <patrick.miviere@gmail.com>
2025-07-19 23:49:22 +0200

    output for stop

--- ec2-instance/outputs.tf
+++ ec2-instance/outputs.tf
@@ -2,3 +2,8 @@ output "instance_public_ip" {
   description = "Adresse IP publique de l'instance EC2"
   value       = aws_instance.ec2.public_ip
 }
+
+output "aws_region" {
+  value = var.aws_region
+}
+
